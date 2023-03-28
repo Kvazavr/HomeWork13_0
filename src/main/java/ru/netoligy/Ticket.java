48073a25373d6695b94fcd0e31d0c2bc14d44411
Elena Nekrasova <kvazavr@yandex.ru>
2023-03-28 16:20:55 +0400

    Modified code

--- src/main/java/ru/netoligy/Ticket.java
+++ src/main/java/ru/netoligy/Ticket.java
@@ -51,7 +51,9 @@ public class Ticket {
         Ticket ticket = (Ticket) o;
         return price == ticket.price && timeFrom == ticket.timeFrom && timeTo == ticket.timeTo && from.equals(ticket.from) && to.equals(ticket.to);
     }
-    Ticket ticket1 = new Ticket("Moscow","Saratov",3600,16,18);            ;
+
+    Ticket ticket1 = new Ticket("Moscow", "Saratov", 3600, 16, 18);
+    ;
     Ticket ticket2 = new Ticket("Moscow", "Saint-Petersburg", 2100, 14, 16);
     Ticket ticket3 = new Ticket("Sakhalin", "Samara", 42_000, 7, 17);
     Ticket ticket4 = new Ticket("Samara", "Omsk", 7_500, 9, 14);
@@ -62,7 +64,18 @@ public class Ticket {
         return Objects.hash(from, to, price, timeFrom, timeTo);
     }
 
-    TicketTimeComparator timeComparator = new TicketTimeComparator();
-    Ticket[] tickets = { ticket1, ticket2, ticket3, ticket4 };
-            Arrays.sort (tickets,timeComparator);
+    //    TicketTimeComparator timeComparator = new TicketTimeComparator();
+//    Ticket[] tickets = { ticket1, ticket2, ticket3, ticket4 };
+//            Arrays.sort (tickets, timeComparator);
+//
+    @Override
+    public int compareTo(Ticket o) {
+        if (this.price < o.price) {
+            return -1;
+        } else if (this.price > o.price) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
--- src/main/java/ru/netoligy/TicketTimeComparator.java
+++ src/main/java/ru/netoligy/TicketTimeComparator.java
@@ -10,9 +10,9 @@ public class TicketTimeComparator implements Comparator<Ticket> {
 
     @Override
     public int compare(Ticket t1, Ticket t2) {
-        if (t1.FlyTime < t2.FlyTime) {
+        if (t1.flyTime() < t2.flyTime()) {
             return -1;
-        } else if (t1.FlyTime > t2.FlyTime()) {
+        } else if (t1.flyTime() > t2.flyTime()) {
             return 1;
         } else {
             return 0;
